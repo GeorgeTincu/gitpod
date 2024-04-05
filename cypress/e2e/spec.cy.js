9d97cb60549b45cee6631ec32d83dcd04e7cc3cd
George <george.tincu@fxbits.io>
2024-04-05 09:25:22 +0300

    continuare gitpod

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("https://example.cypress.io");
+    cy.get(".navbar-nav>li")
+      .eq(0)
+      .click()
+      .should("have.class", "dropdown open")
+      .and("be.visible");
+    cy.get(".dropdown-menu>li").eq(2).click();
+    cy.get(".dropdown-menu>li").eq(2).should("have.class", "active");
+    cy.get("#password1").focus().should("have.class", "focus");
+    cy.get(".action-form").type("mama").submit();
+    cy.get(".action-form")
+      .next()
+      .should("have.text", "Your form has been submitted!");
+  });
+});
